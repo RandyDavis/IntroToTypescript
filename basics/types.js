7f473043c47d2811450a5ddded2ddbd1ca4d284c
Randy Davis <randy_davis@outlook.com>
2015-07-27 22:36:14 -0500

    finish list of basics examples

--- basics/types.js
+++ basics/types.js
@@ -1,2 +1,30 @@
-var n = "Hey";
-n = "Rob";
+//var n: String = "Hey";
+//
+//n = "Rob";
+// Booleans
+var isWinter = false;
+//isWinter = 123;
+// Number
+var count = 5;
+// String
+var name = "Brand";
+// Arrays
+// Can only add strings to this array
+var names = ["Randy", "Jim", "Arya"];
+// Can add any type to this array
+var moreNames = ["Joe", 2, false, "Bill", null];
+// Enum
+var Starks;
+(function (Starks) {
+    Starks[Starks["Randy"] = 0] = "Randy";
+    Starks[Starks["Jon"] = 1] = "Jon";
+    Starks[Starks["Brad"] = 2] = "Brad";
+    Starks[Starks["Kate"] = 3] = "Kate";
+    Starks[Starks["Bill"] = 4] = "Bill";
+})(Starks || (Starks = {}));
+;
+var buffalo = Starks.Bill;
+// Void
+function getName() {
+    console.log("Winter is coming");
+}
--- basics/types.ts
+++ basics/types.ts
@@ -1,3 +1,29 @@
-var n: String = "Hey";
+//var n: String = "Hey";
+//
+//n = "Rob";
 
-n = "Rob";
+// Booleans
+var isWinter: Boolean = false;
+//isWinter = 123;
+
+// Number
+var count: Number = 5;
+
+// String
+var name: string = "Brand";
+
+// Arrays
+// Can only add strings to this array
+var names: string[] = ["Randy", "Jim", "Arya"];
+
+// Can add any type to this array
+var moreNames: any[] = ["Joe", 2, false, "Bill", null];
+
+// Enum
+enum Starks { Randy, Jon, Brad, Kate, Bill };
+var buffalo: Starks = Starks.Bill;
+
+// Void
+function getName(): void {
+	console.log("Winter is coming");
+}
